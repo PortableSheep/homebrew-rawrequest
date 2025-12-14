db3fd056dc23b660bed8abb3461d401298fe9299
Michael Gunderson <michael@portablesheep.com>
2025-12-13 19:37:07 -0600

    Swapped formula for cask

--- Casks/rawrequest.rb
+++ Casks/rawrequest.rb
@@ -1,59 +1,25 @@
 # typed: false
 # frozen_string_literal: true
 
-# Homebrew formula for RawRequest
-# To install from this repo directly:
-#   brew install --formula ./Formula/rawrequest.rb
-#
-# Or tap this repo:
-#   brew tap portablesheep/rawrequest https://github.com/portablesheep/RawRequest
-#   brew install rawrequest
-
-class Rawrequest < Formula
-  desc "A modern, lightweight HTTP client for developers"
-  homepage "https://github.com/portablesheep/RawRequest"
+cask "rawrequest" do
   version "1.0.0"
-  license "MIT"
+  sha256 "7336f34292ba7739c25e09304dee7ef9a444df39e56a9caabc9d2a882527271b"
 
-  # Update these URLs when releasing
-  # The sha256 will be output by the package_homebrew.sh script
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/portablesheep/RawRequest/releases/download/v#{version}/RawRequest-v#{version}-macos-universal.tar.gz"
-      sha256 "7336f34292ba7739c25e09304dee7ef9a444df39e56a9caabc9d2a882527271b"
-    else
-      url "https://github.com/portablesheep/RawRequest/releases/download/v#{version}/RawRequest-v#{version}-macos-universal.tar.gz"
-      sha256 "7336f34292ba7739c25e09304dee7ef9a444df39e56a9caabc9d2a882527271b"
-    end
+  url "https://github.com/portablesheep/RawRequest/releases/download/v#{version}/RawRequest-v#{version}-macos-universal.tar.gz"
+  name "RawRequest"
+  desc "Modern, lightweight HTTP client for developers"
+  homepage "https://github.com/portablesheep/RawRequest"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
 
-  def install
-    # Install the app bundle to the prefix
-    prefix.install "RawRequest.app"
-    
-    # Create a symlink in bin for CLI access (optional)
-    if File.exist?("rawrequest")
-      bin.install "rawrequest"
-    end
-  end
+  app "RawRequest.app"
 
-  def caveats
-    <<~EOS
-      RawRequest.app has been installed to:
-        #{prefix}/RawRequest.app
-
-      To open RawRequest, you can:
-        1. Run: open #{prefix}/RawRequest.app
-        2. Or add it to your Applications folder:
-           ln -sf #{prefix}/RawRequest.app /Applications/RawRequest.app
-
-      On first launch, macOS may show a security warning.
-      Go to System Preferences > Security & Privacy to allow the app.
-    EOS
-  end
-
-  test do
-    # Basic test to verify the app bundle exists
-    assert_predicate prefix/"RawRequest.app", :exist?
-  end
+  zap trash: [
+    "~/Library/Application Support/rawrequest",
+    "~/Library/Preferences/com.rawrequest.app.plist",
+    "~/Library/Caches/rawrequest",
+  ]
 end
